c1d7d0d6291c87bf6d81d488e079dce3838fa7dd
swordfest <alexanderhavarro@gmail.com>
2022-12-04 01:28:20 -0500

    adding types

--- pages/components/functions.tsx
+++ pages/components/functions.tsx
@@ -1,26 +1,27 @@
 
 
-export async function localArray(arr) {
-	var country = new Object();
-	var world = { code: "UN", name: "Worldwide" };
-	var codeString = new Array();
-	codeString.push(world);
-	for (i = 0; i < arr.length; i++) {
-		var countryNames = await getCountry(arr[i]);
-		country = { code: arr[i], name: countryNames };
-		codeString.push(country);
-	}
-	return codeString;
-}
+// export async function localArray(arr: any) {
+// 	var country = new Object();
+// 	var world = { code: "UN", name: "Worldwide" };
+// 	var codeString = new Array();
+// 	codeString.push(world);
+// 	var i: number
+// 	for (i = 0; i < arr.length; i++) {
+// 		var countryNames = await getCountry(arr[i]);
+// 		country = { code: arr[i], name: countryNames };
+// 		codeString.push(country);
+// 	}
+// 	return codeString;
+// }
 
-export function getFlagEmoji(countryCode) {
+export function getFlagEmoji(countryCode: any) {
 	const codePoints = countryCode
 		.toUpperCase()
 		.split("")
-		.map((char) => 127397 + char.charCodeAt());
+		.map((char: any) => 127397 + char.charCodeAt());
 	return String.fromCodePoint(...codePoints);
 }
 
-export function getPercentage(portion, total) {
+export function getPercentage(portion: number, total: number) {
 	return Math.trunc((portion * 100) / total);
 }
