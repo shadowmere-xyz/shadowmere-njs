99b76b2f70a533b8b489ebe4bada105cdd8c9960
swordfest <alexanderhavarro@gmail.com>
2022-12-04 01:42:46 -0500

    yet fixing types

--- pages/components/server.tsx
+++ pages/components/server.tsx
@@ -1,7 +1,7 @@
 import { getFlagEmoji, getPercentage } from './functions';
 import React, {useEffect, useState} from 'react';
 
-export default function Server (dataServer: object, {handleQR}: any) {
+export default function Server (dataServer:{proxy: any, handleQR: any}) {
     const [timeCopy, setTimeCopy] = useState(false)
 
     return (
