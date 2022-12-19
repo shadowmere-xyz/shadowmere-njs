32a1d3dd40db85bc00d30a3eec1897aea00228bf
Alex Navarro <alexanderhavarro@gmail.com>
2022-12-19 10:09:20 -0800

    fixing things

--- pages/subscription.tsx
+++ pages/subscription.tsx
@@ -10,14 +10,14 @@ export default function Sub() {
 				<div className="contenido-sub col-span-12 xl:col-span-9 w-full h-fit bg-white dark:bg-[#212121] dark:text-[#cfcfcf] rounded-lg shadow-lg flex flex-col gap-6 p-8 mb-4 2xl:mb-40">
 					<h1 className="text-2xl font-bold">Server list subscription</h1>
 					<p>
-						Some applications like
+						Some applications like 
 						<a
 							href="https://play.google.com/store/apps/details?id=com.github.shadowsocks"
 							className="text-[#579eff] font-bold after:content-['\f08e'] hover:text-[#467ecc] transition-colors after:pl-1 after:font-awesome after:text-sm">
 							Shadowsocks for Android
 						</a>
-						allow users to subscribe to a list of servers. You can subscribe to
-						Shadowmere&apos;s using this link:
+						 allow users to subscribe to a list of servers. You can subscribe to
+						Shadowmere&apos;s using this link: 
 						<span
 							onClick={() => {
 								navigator.clipboard.writeText(
