030c5aafd608baa67dd3591e4bc99011f8f41440
swordfest <alexanderhavarro@gmail.com>
2023-03-17 15:02:29 -0400

    refining the UI

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,11 +21,11 @@ export default function Home() {
 
 	return (
 		<>
-			<div className="lista-servers-vpns col-span-12 xl:col-span-9 w-full h-full flex flex-col gap-4">
+			{/* <div className="lista-servers-vpns col-span-12 xl:col-span-9 w-full h-full flex flex-col gap-4">
 				<Pagination isVisible={true} />
 				<ServersList />
 				<Pagination isVisible={true} />
-			</div>
+			</div> */}
 		</>
 	);
 }
