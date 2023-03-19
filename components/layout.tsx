0674a88652806ec3cf2fdefeaf8cbb0e5a922bd5
swordfest <alexanderhavarro@gmail.com>
2023-03-19 18:59:28 -0400

    hotfix for ModalQR component

--- components/layout.tsx
+++ components/layout.tsx
@@ -84,7 +84,7 @@ export default function Layout({ children }: any) {
 			{/* <Loader isLoading={loader} /> */}
 			<Meta />
 			<Navbar />
-			<ModalQR qrActive={qrScreenCode} />
+			{qrScreenCode && <ModalQR />}
 			<main className="wrapper container mx-auto w-full h-full xl:h-full grid grid-cols-12 auto-rows-auto mb-16 2xl:mb-0 mt-20 py-6 xl:py-8 gap-[30px] px-4 pb-4">
 				<Sidebar data={proxies} time={time} />
 				{children}
--- components/modalQR.tsx
+++ components/modalQR.tsx
@@ -5,11 +5,14 @@ import { useRecoilState } from 'recoil'
 export default function ModalQR (props: any) {
     const [proxID, setProxID] = useRecoilState(proxyId)
     const [qrActive, setQrActive] = useRecoilState(qrScreen)
+
     const handleClick = () => {
         setQrActive(false)
     }
+
     return (
-        <div onClick={handleClick} className={"modal-qr w-full fixed z-[200] left-0 top-0 h-screen bg-black bg-opacity-[0.7] flex flex-col items-center justify-center gap-4 " + (props.qrActive ? '' : 'hidden')}>
+        <div onClick={handleClick} className={"modal-qr w-full fixed z-[200] left-0 top-0 h-screen bg-black bg-opacity-[0.7] flex flex-col items-center justify-center gap-4 "}>
+        {/* <div onClick={handleClick} className={"modal-qr w-full fixed z-[200] left-0 top-0 h-screen bg-black bg-opacity-[0.7] flex flex-col items-center justify-center gap-4 " + (props.qrActive ? '' : 'hidden')}> */}
             <div className="w-auto h-auto flex flex-col gap-4 justify-center ">
                 <div className=" w-[328px] h-auto sm:w-[360px] bg-white rounded-lg flex flex-col gap-4">
                     <img src={'https://shadowmere.akiel.dev/' + proxID?.toString() + '/qr'} className=" rounded-lg" alt="QR Code for server address"/>
