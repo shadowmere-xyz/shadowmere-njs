f9ee907832eab6fb2ca25c8eb89df5f25be84c5e
swordfest <alexanderhavarro@gmail.com>
2023-03-30 09:36:31 -0400

    testing some configs on next.config

--- next.config.js
+++ next.config.js
@@ -3,12 +3,12 @@ const nextConfig = {
   distDir: 'dist',
   reactStrictMode: true,
   swcMinify: true,
-  compiler: {
-    styledComponents: {
-      displayName: true,
-      ssr: true
-    },
-  },
+  // compiler: {
+  //   styledComponents: {
+  //     displayName: true,
+  //     ssr: true
+  //   },
+  // },
   staticPageGenerationTimeout: 1000,
   // output: 'standalone',
 }
