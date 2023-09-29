311e5684904fe0c67d8850c72d5de02dc6849080
Jorge Alberto Diaz Orozco (Akiel) <diazorozcoj@gmail.com>
2023-09-29 23:27:27 +0200

    Remove script types
    apparently not needed

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,10 +13,10 @@ export default function App({Component, pageProps}: AppProps) {
     return (
         <RecoilRoot>
             <ThemeProvider enableSystem={true} defaultTheme={'system'} attribute="class">
-                <Script type="text/javascript"
+                <Script strategy='afterInteractive'
                         src="https://www.termsfeed.com/public/cookie-consent/4.1.0/cookie-consent.js"
                         charset="UTF-8"></Script>
-                <Script id={`cookie_consent`} type="text/javascript" charset="UTF-8"
+                <Script id={`cookie_consent`} strategy='afterInteractive'
                         dangerouslySetInnerHTML={{
                             __html: `
                     document.addEventListener('DOMContentLoaded', function () {
@@ -34,7 +34,7 @@ export default function App({Component, pageProps}: AppProps) {
                 });`
                         }}/>
 
-                <Script type="text/plain" data-cookie-consent="tracking" async src="https://umami.akiel.dev/script.js"
+                <Script strategy='afterInteractive' data-cookie-consent="tracking" async src="https://umami.akiel.dev/script.js"
                         data-website-id="ab0233a0-d8c9-49a3-ba07-00c2adcbc9cc"></Script>
                 <Layout>
                     <Component {...pageProps} />
