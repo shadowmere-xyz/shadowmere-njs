a303875def1a59acd4acb14959362587d27da968
swordfest <alexanderhavarro@gmail.com>
2024-02-13 14:57:54 -0500

    update import icons

--- components/serverCol.tsx
+++ components/serverCol.tsx
@@ -4,7 +4,13 @@ import { proxyId, qrScreen, viewDataLayout } from "../libs/store";
 import { useRecoilState } from "recoil";
 import useMousePosition from "../libs/useMousePosition";
 import { useEffectOnce } from "usehooks-ts";
-import { IconCheck, IconClipboard, IconDownload, IconQrcode } from "@tabler/icons-react";
+import {
+  IconAnchor,
+  IconCheck,
+  IconClipboard,
+  IconDownload,
+  IconQrcode,
+} from "@tabler/icons-react";
 
 export default function ServerCol(dataServer: { proxy: any }) {
   const [timeCopy, setTimeCopy] = useState(false);
@@ -120,18 +126,7 @@ export default function ServerCol(dataServer: { proxy: any }) {
 
           <div className="w-16 h-6 before:content-[''] before:absolute before:rounded before:w-16 before:h-6 before:bg-[#7474FF] flex gap-1 items-center justify-start select-none bg-[#cfcfcf] font-medium text-xs sm:text-sm rounded overflow-visible">
             <div className="w-full h-full flex items-center justify-center z-10 gap-0.5 ">
-              <svg
-                width="12"
-                height="12"
-                viewBox="0 0 12 12"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M11 7.5L9.5 5.5L8 7.5H8.953C8.735 8.7345 7.734 9.7355 6.5 9.953V6.5H7.5V5.5H6.5V4.668C7.2715 4.4385 7.857 3.7065 7.857 2.8575C7.857 1.833 7.024 1 6 1C4.976 1 4.143 1.833 4.143 2.8575C4.143 3.7065 4.7285 4.4385 5.5 4.668V5.5H4.5V6.5H5.5V9.953C4.2655 9.7355 3.2645 8.7345 3.047 7.5H4L2.5 5.5L1 7.5H2.0365C2.292 9.4425 4.001 11 6 11C7.999 11 9.708 9.4425 9.9635 7.5H11ZM5.143 2.8575C5.143 2.385 5.5275 2 6 2C6.4725 2 6.857 2.385 6.857 2.8575C6.857 3.333 6.4565 3.75 6 3.75C5.5435 3.75 5.143 3.333 5.143 2.8575Z"
-                  fill="white"
-                />
-              </svg>
+              <IconAnchor size={14} strokeWidth={2.5} color="white" />
               <span className="w-auto h-6 text-white text-xs sm:text-sm font-semibold flex items-center justify-center overflow-hidden">
                 {dataServer.proxy.port}
               </span>
@@ -168,7 +163,7 @@ export default function ServerCol(dataServer: { proxy: any }) {
 
         <button
           onClick={handleQR}
-		  data-umami-event={"Mostrar QR Server"}
+          data-umami-event={"Mostrar QR Server"}
           title="QR Code"
           className="opt-codigo-qr w-4 h-4 sm:w-10 sm:h-10 rounded-md flex items-center justify-center cursor-pointer hover:bg-[#EBEBEB] dark:hover:bg-[#1B1B1B] active:bg-[#D7D7D7] dark:active:bg-[#111111] transition-all duration-150 active:scale-[95%]"
         >
@@ -180,10 +175,10 @@ export default function ServerCol(dataServer: { proxy: any }) {
             "https://shadowmere.akiel.dev/" + dataServer.proxy.id + "/config"
           }
           title="Download"
-		  data-umami-event={"Descarga Server"}
+          data-umami-event={"Descarga Server"}
           className="opt-descargar w-4 h-4 sm:w-10 sm:h-10 rounded-md flex items-center justify-center cursor-pointer hover:bg-[#EBEBEB] dark:hover:bg-[#1B1B1B] active:bg-[#D7D7D7] dark:active:bg-[#111111] transition-all duration-150 active:scale-[95%]"
         >
-          <IconDownload/>
+          <IconDownload />
         </a>
       </div>
     </div>
