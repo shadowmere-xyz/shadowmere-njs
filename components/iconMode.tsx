086520cd8a1c02054cd9a355643cd5dd70638e90
swordfest <alexanderhavarro@gmail.com>
2024-02-26 00:48:55 -0500

    setting default value

--- components/iconMode.tsx
+++ components/iconMode.tsx
@@ -1,21 +1,27 @@
 import { IconDeviceImac, IconMoon, IconSun } from "@tabler/icons-react";
 
-export default function Icon (props: {name: String | undefined, rotate: boolean}){
-	props.name ?? 'system'
-	switch (props.name!) {
-		case 'light': {
-			return <IconSun className={`stroke-[#303030] dark:stroke-[#cfcfcf]`} />
-		}
+export default function Icon(props: {
+  name: String | undefined;
+  rotate: boolean;
+}) {
+  switch (props.name) {
+    case "light": {
+      return <IconSun className={`stroke-[#303030] dark:stroke-[#cfcfcf]`} />;
+    }
 
-		case 'dark': {
-			return <IconMoon className={`stroke-[#303030] dark:stroke-[#cfcfcf]`} />
-		}
+    case "dark": {
+      return <IconMoon className={`stroke-[#303030] dark:stroke-[#cfcfcf]`} />;
+    }
 
-		case 'system': {
-			return <IconDeviceImac className={`stroke-[#303030] dark:stroke-[#cfcfcf]`} />
-		}
+    case "system": {
+      return (
+        <IconDeviceImac className={`stroke-[#303030] dark:stroke-[#cfcfcf]`} />
+      );
+    }
 
-		default:
-			break;
-	}
+    default:
+      return (
+        <IconDeviceImac className={`stroke-[#303030] dark:stroke-[#cfcfcf]`} />
+      );
+  }
 }
